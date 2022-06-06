ecb7e2e540b33e1b22d59082634c08dee0bfea28
namesty <nestor09amesty@gmail.com>
2022-06-06 22:35:25 +0200

    (chore): deprecated hangzhounet

--- registrydao/constants.py
+++ registrydao/constants.py
@@ -1,7 +1,6 @@
 WHITE_BETTER_CALL_DEV_API = 'https://white.better-call.dev/v1'
 BETTER_CALL_DEV_API = 'https://api.better-call.dev/v1'
 BCD_NETWORK_MAP = {
-  "hangzhounet": "hangzhou2net",
   "mainnet": "mainnet",
   "ithacanet": "ithacanet"
 }
--- registrydao/utils/ctx.py
+++ registrydao/utils/ctx.py
@@ -3,9 +3,6 @@ from dipdup.context import HandlerContext
 
 def extract_network_from_ctx(ctx: HandlerContext) -> str:
     datasource_url = ctx.datasource._http._url
-
-    if 'hangzhou2net' in datasource_url:
-        return 'hangzhounet'
     
     if 'mainnet' in datasource_url:
         return 'mainnet'
