d7454a1d7aa70ea473189f8bd718eb028573618e
Manank Patni <manank321@gmail.com>
2022-08-20 05:06:19 +0530

    Add Ghostnet Support
    Signed-off-by: Manank Patni <manank321@gmail.com>

--- registrydao/constants.py
+++ registrydao/constants.py
@@ -2,5 +2,5 @@ WHITE_BETTER_CALL_DEV_API = 'https://white.better-call.dev/v1'
 BETTER_CALL_DEV_API = 'https://api.better-call.dev/v1'
 BCD_NETWORK_MAP = {
   "mainnet": "mainnet",
-  "jakartanet": "jakartanet"
+  "ghostnet": "ghostnet"
 }
