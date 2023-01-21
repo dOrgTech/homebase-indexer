3da56f30ef11ce7bd98d7c85639fea53ce797563
Manank Patni <manank321@gmail.com>
2023-01-21 18:41:46 +0530

    Add staked field to Vote Model to support unstake from all proposals
    Signed-off-by: Manank Patni <manank321@gmail.com>

--- registrydao/handlers/on_unstake_vote.py
+++ registrydao/handlers/on_unstake_vote.py
@@ -16,7 +16,22 @@ async def on_unstake_vote(
 ) -> None:
     try:
         dao_address = unstake_vote.data.target_address
+        proposal_key = unstake_vote.parameter.__root__[0]
+
+        dao = await models.DAO.get(address=dao_address)
+        proposal = await models.Proposal.get(key=proposal_key, dao=dao)
+
+        voter = await models.Holder.get_or_create(address=unstake_vote.data.sender_address)
+
         await update_ledger(dao_address, unstake_vote.data.diffs)
+
+        votes = await models.Vote.filter(proposal=proposal, voter=voter[0])
+
+        for vote in votes:
+            vote.staked = False
+            await vote.save()
+
     except Exception as e:
-        print("Error in on_unstake_vote: " + str(unstake_vote.data.target_address))
+        print("Error in on_unstake_vote: " +
+              str(unstake_vote.data.target_address))
         print(e)
